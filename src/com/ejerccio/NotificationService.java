8908d8655178649437c2390659b2bfb38bc5ba8b
gabriel martinmulas devicente <gmmdv6@gmail.com>
2026-02-27 19:36:30 +0100

    primera refactor

--- src/com/ejerccio/NotificationService.java
+++ src/com/ejerccio/NotificationService.java
@@ -2,12 +2,16 @@ package com.ejerccio;
 
 public class NotificationService {
 
+	private static final String TYPE_PUSH = "push";
+	private static final String TYPE_SMS = "sms";
+	private static final String TYPE_EMAIL = "email";
+
 	public void enviarNotificacion(String tipo, String mensaje, String destinatario) {
-        if (tipo.equals("email")) {
+        if (tipo.equals(TYPE_EMAIL)) {
             System.out.println("Enviando email a " + destinatario + ": " + mensaje);
-        } else if (tipo.equals("sms")) {
+        } else if (tipo.equals(TYPE_SMS)) {
             System.out.println("Enviando SMS a " + destinatario + ": " + mensaje);
-        } else if (tipo.equals("push")) {
+        } else if (tipo.equals(TYPE_PUSH)) {
             System.out.println("Enviando push a " + destinatario + ": " + mensaje);
         }
     }
